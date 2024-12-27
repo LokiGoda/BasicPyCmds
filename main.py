51adea753c6f780fe0dc1710cbc9e1b783b5ab7f
LokiGoda <arslan1980awan@gmail.com>
2024-12-27 09:53:49 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,188 @@
+# HelloWorld
+# print("Hello World")
+
+#                                                  VARIABLES
+# price = 10
+# print(price)
+
+# integer
+# price = 10
+
+# floating point number or float
+# rating = 4.9
+
+# string
+# name = 'Loki'
+
+# boolean
+# is_published = False
+# print(price)
+
+# We check in a patient John Smith. He is 20 years old and is a new patient
+
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+
+# Input from the user
+
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+# Exercise: Input from the user, ask two questions: person's name and favorite color. Then print a message like "
+# Loki likes red"
+
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
+
+# Exercise: ask the birthdate and calculate age
+
+# birth_year = input ('Birth year: ')
+# age = 2024 - int(birth_year)
+# print(age)
+
+# Exercise: Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+# weight_lbs = input ('Your weight in pounds: ')
+# eight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+#                                                         STRINGS
+
+# course = 'Python for Beginners'
+# print(course)
+
+# Multiple line Quotes
+
+# email = '''
+# Hello
+# How are you
+# Regards
+# '''
+# print (email)
+
+# How to give index (get first charactor)
+
+# course = 'Python for Beginners'
+# print(course[0])
+
+# How to give index (get more charactor), it excludes the index 3rd charactor
+
+# course = 'Python for Beginners'
+# print(course[0:3])
+
+# How to give index (get all characters)
+
+# course = 'Python for Beginners'
+# print(course[0:])
+
+# name = 'Jennifer'
+# print(name [1:-1])
+
+# FORMATTED STRINGS
+
+# first_name = 'John'
+# last_name = 'Smith'
+# message = first_name + ' [' + last_name + '] is a coder'
+# OR below is more ideal way (formatted strings)
+# msg = f'{first_name} [{last_name}] is a coder '
+# print(msg)
+
+# STRING METHODS
+
+# course = 'Python for Beginners'
+# calculate number of alphabets
+# print(len(course))
+
+# course = 'Python for Beginners'
+# method to change the characters to uppercase
+# print(course.upper())
+
+# Find a character, this will return an index number, for below example, it will return 0 because P is a 0
+# in index sequence. Also, remember that find is case-sensitive.
+
+# course = 'Python for Beginners'
+# print(course.find('O'))
+
+# Replace characters. It is case sensitive
+# course = 'Python for Beginners'
+# print(course.replace('Beginners', 'Absolute Beginners'))
+
+# Boolean, if a string contains a word, remember that is a case-sensitive.
+# course = 'Python is for Beginner'
+# print('Python' in course)
+
+#                                            ARITHMETIC OPERATIONS
+
+# Integers and Floats (decimal)
+
+# print(10+3)
+# print(10-3)
+# print(10/3) # this way of division returns a float number
+# print(10//3) # this way of division returns a whole number or an integer
+# print(10%3) # this returns the remainder of the division and it not a percentage value
+# print(10**3) # this returns an exponent
+
+# x = 10
+# x = x + 3
+# print(x)
+
+# Augmented assignment operator
+# x = 10
+# x += 3
+# print(x)
+
+# x = 10
+# x -= 3
+# print(x)
+
+# Operator Precedence (Parentheses, exponent, Multiply, divide, add and subtract)
+# x = 10+3*2
+# print(x)
+
+# x = 10+3*2**2
+# print(x)
+
+# x = (10+3)*2**2
+# print(x)
+
+# x = (2+3)*10-3
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
